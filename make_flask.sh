99cf3d89dbbafe3bf2409d947b7a67ea905c20ee
networkdavit <lonecreation1612@gmail.com>
2021-02-08 15:48:38 +0400

    Making changes in make flask script

--- make_flask.sh
+++ make_flask.sh
@@ -2,7 +2,12 @@
 name="$1"
 mkdir $name
 cd $name
-touch app.py 
+echo "from flask import Flask" >> app.py
+echo "app = Flask(__name__)" >> app.py
+echo "" >> app.py
+echo "@app.route('/')" >> app.py
+echo "def hello_world():" >> app.py
+echo "    return \"Hello World\"" >> app.py 
 echo -e "FLASK_APP=app.py\nFLASK_ENV=development"  > .env
 mkdir templates static
 python3 -m venv venv
